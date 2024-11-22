a00720798e4a8fafd0cd503272cf12141bf81b9f
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-11-22 19:01:45 +0100

    creation du snake

--- main.py
+++ main.py
@@ -9,10 +9,27 @@ GAME_SPEED = 100
 SNAKE_COLOR = "green"
 FOOD_COLOR = "red"
 BACKGROUND_COLOR = "black"
+direction = "Right"
+snake_pos = [(200, 200), (190, 200), (180, 200)]
 
 root = tk.Tk()
 root.title("Snake")
 root.geometry("{}x{}".format(GAME_WIDTH, GAME_HEIGHT))
 root.resizable(width=False, height=False)
+
+canvas = tk.Canvas(root, width=GAME_WIDTH, height=GAME_HEIGHT, background=BACKGROUND_COLOR)
+canvas.pack()
+
+for x,y in snake_pos:
+    snake = canvas.create_rectangle(x,y, x + SQUARE_SIZE, y + SQUARE_SIZE, fill="blue")
+
+def move_snake():
+    print("Move")
+
+def update():
+    move_snake()
+    root.after(GAME_SPEED, update)
+
+root.after(GAME_SPEED, update)
 
 root.mainloop()
